e68c1d03b8f012d9a583c26e5c531eb8e64a4f10
D1plo1d <d1plo1d@d1plo1d.com>
2015-08-14 20:27:24 -0400

    Fixing error horizontal alignment

--- src/javascripts/frigging_bootstrap/components/errors.js
+++ src/javascripts/frigging_bootstrap/components/errors.js
@@ -8,11 +8,11 @@ export default class extends React.Component {
   static defaultProps = require("../default_props.js")
 
   render() {
-    return div({className: "row"},
+    return div({},
       this.props.errors.map((error) => {
         return [
-          div({className: "col-xs-12 col-sm-12 col-md-12 col-lg-12"},
-            div({className: "frigging-bootstrap-error", ref: `error-${error}`},
+          div({className: "col-xs-12"},
+            div({className: "frigb-error", ref: `error-${error}`},
               div({className: "alert alert-danger"},
                 i({className:"fa fa-exclamation-circle"}),
                 span({className: "sr-only"}, "Error:"),
