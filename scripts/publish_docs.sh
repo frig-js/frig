a5b5ac104128c3868ddbd362830671e8acc950ca
D1plo1d <d1plo1d@d1plo1d.com>
2015-09-07 19:04:01 -0400

    Adding css to doc examples

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -4,6 +4,7 @@ if [[ -z $(git status -s) ]]
 then
   ./scripts/preversion.sh
   cp ./node_modules/frigging-bootstrap/dist/frigging-bootstrap.js ./dist/frigging-bootstrap.js
+  cp ./node_modules/frigging-bootstrap/dist/frigging-bootstrap.css ./dist/frigging-bootstrap.css
   mkdocs gh-deploy
   git add -A
   git reset --hard
