2f34ac8164f095d412e4bf7aafd39e0c981c5077
D1plo1d <d1plo1d@d1plo1d.com>
2015-09-17 12:32:26 -0400

    Fixing bower versioning

--- scripts/preversion.sh
+++ scripts/preversion.sh
@@ -22,4 +22,6 @@ echo ""
 echo "FRIG: UPDATING BOWER VERSION"
 echo "======================================================="
 VERSION=`npm version|grep -e "frig: '[0-9\.]*"|grep -E [0-9\.]+|grep -oE "[0-9\.]+"`
-sed -i -e "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" bower.json
+sed -i.bak -e "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" bower.json
+rm bower.json.bak
+echo "DONE"
