60104b89627ed264c2d27c240a049f7f61f0beb6
D1plo1d <d1plo1d@d1plo1d.com>
2015-09-21 10:28:40 -0400

    Including bower.json in the version commit

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,6 +4,7 @@ echo "======================================================="
 VERSION=`npm version|grep -e "frig: '[0-9\.]*"|grep -E [0-9\.]+|grep -oE "[0-9\.]+"`
 sed -i.bak -e "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" bower.json
 rm bower.json.bak
+git add bower.json
 echo "DONE"
 
 git add -A dist
