1445d63677526224ca8bf951eeb133ce164464e9
D1plo1d <d1plo1d@d1plo1d.com>
2015-10-16 15:32:14 -0400

    Updating version script for npm3

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,7 +1,7 @@
 echo ""
 echo "FRIG: UPDATING BOWER VERSION"
 echo "======================================================="
-VERSION=`npm version|grep -e "frig: '[0-9\.]*"|grep -E [0-9\.]+|grep -oE "[0-9\.]+"`
+VERSION=`npm version|grep -e "frig[^:]*: '[0-9\.]*"|grep -E [0-9\.]+|grep -oE "[0-9\.]+"`
 sed -i.bak -e "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" bower.json
 rm bower.json.bak
 git add bower.json
