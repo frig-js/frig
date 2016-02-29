79f73ff168fe6bff05e212947bb27a16027b46ce
Rob Gilson <thatotherdude@gmail.com>
2016-02-29 14:38:00 -0500

    Add factories for coffeescript users

---
+++ src/factories.js
@@ -0,0 +1,5 @@
+import Form from "./components/form.js"
+import Input from "./components/input.js"
+
+export const form = React.createFactory(Form)
+export const input = React.createFactory(Input)
--- src/index.js
+++ src/index.js
@@ -6,27 +6,31 @@ import Fieldset from "./components/fieldset.js"
 import FieldsetText from "./components/fieldset_text.js"
 import ValueLinkedSelect from "./components/value_linked_select.js"
 import util from "./util.js"
+import typeMapping from "./type_mapping.js"
 
-module.exports = {
+const HigherOrderComponents = {
+  Boolean: require("./higher_order_components/boolean.js"),
+  Focusable: require("./higher_order_components/focusable.js"),
+}
+
+// Setter and getter for the Frig default theme
+function defaultTheme(theme) {
+  if (theme == null) return Form.defaultProps.theme
+  if (theme.component == null) throw "Invalid theme. Expected an object"
+  Form.originalClass.defaultProps.theme = theme
+  Input.originalClass.defaultProps.theme = theme
+}
+
+export default {
+  defaultTheme,
   Form,
   Input,
+  Submit,
   FormErrorList,
   Fieldset,
   FieldsetText,
-  FormErrorList,
-  Submit,
-  util,
   ValueLinkedSelect,
-  typeMapping: require("./type_mapping.js"),
-  HigherOrderComponents: {
-    Boolean: require("./higher_order_components/boolean.js"),
-    Focusable: require("./higher_order_components/focusable.js"),
-  },
-  // Setter and getter for the Frig default theme
-  defaultTheme(theme) {
-    if (theme == null) return Form.defaultProps.theme
-    if (theme.component == null) throw "Invalid theme. Expected an object"
-    Form.originalClass.defaultProps.theme = theme
-    Input.originalClass.defaultProps.theme = theme
-  },
+  HigherOrderComponents,
+  util,
+  typeMapping,
 }
