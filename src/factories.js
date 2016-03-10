96b0b6b607f0d83cbb934db1a59d9f5188efacf7
Jesse Buchanan <hello@jessebuchanan.ca>
2016-03-10 15:30:58 -0500

    add factories for Submit and FormErrorList

--- src/factories.js
+++ src/factories.js
@@ -1,5 +1,9 @@
 import Form from "./components/form.js"
 import Input from "./components/input.js"
+import Submit from "./components/submit.js"
+import FormErrorList from "./components/form_error_list.js"
 
 export const form = React.createFactory(Form)
 export const input = React.createFactory(Input)
+export const submit = React.createFactory(Submit)
+export const formErrorList = React.createFactory(FormErrorList)
