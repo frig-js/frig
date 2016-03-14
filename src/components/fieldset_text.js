45558cdf17a9dd73eeb5582b84ea95bebfd97afe
Jason Stathopulos <jstathopulos@touchbistro.com>
2016-03-14 14:22:39 -0400

    Fix Context In Fieldset Text Component

--- src/components/fieldset_text.js
+++ src/components/fieldset_text.js
@@ -3,6 +3,10 @@ import React from "react"
 export default class FieldsetText extends React.Component {
   displayName = "Frig.FieldsetText"
 
+  static contextTypes = {
+    frigFieldset: React.PropTypes.object,
+  }
+
   render() {
     let spanProps = Object.assign({}, this.props)
     delete spanProps.text
