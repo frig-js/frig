4c117ffd1c4ead3e4eccddade8419c8801c01e03
Jason Stathopulos <jstathopulos@touchbistro.com>
2016-04-04 11:28:52 -0400

    Add Fieldset Text Factory For DSL

--- src/factories.js
+++ src/factories.js
@@ -4,9 +4,11 @@ import Input from "./components/input.js"
 import Submit from "./components/submit.js"
 import FormErrorList from "./components/form_error_list.js"
 import Fieldset from "./components/fieldset.js"
+import FieldsetText from "./components/fieldset_text.js"
 
 export const form = React.createFactory(Form)
 export const input = React.createFactory(Input)
 export const submit = React.createFactory(Submit)
 export const formErrorList = React.createFactory(FormErrorList)
 export const fieldset = React.createFactory(Fieldset)
+export const fieldsetText = React.createFactory(FieldsetText)
