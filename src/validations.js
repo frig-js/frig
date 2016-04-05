8545d5188079e4abe45038f893a2edbed6fe8df6
Jesse Buchanan <hello@jessebuchanan.ca>
2016-04-05 11:59:08 -0400

    eslint: 180 on double-quotes (single quotes win)

--- src/validations.js
+++ src/validations.js
@@ -6,19 +6,19 @@ module.exports = {
     if ((props.options || []).filter((o) => o.value == null).length > 0) {
       return undefined
     }
-    if (value != null && value !== "") return undefined
-    return "This field is required."
+    if (value != null && value !== '') return undefined
+    return 'This field is required.'
   },
 
   min(props, opts) {
     const { value } = props.valueLink
-    if (value >= opts || value == null || value === "") return undefined
+    if (value >= opts || value == null || value === '') return undefined
     return `Value cannot be less than ${opts}`
   },
 
   max(props, opts) {
     const { value } = props.valueLink
-    if (value <= opts || value == null || value === "") return undefined
+    if (value <= opts || value == null || value === '') return undefined
     return `Value cannot be greater than ${opts}`
   },
 }
