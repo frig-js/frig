ffc206e80698a120c891fa886c40d8287b34a484
Jesse Buchanan <hello@jessebuchanan.ca>
2016-04-05 11:37:11 -0400

    eslint src/validations.js

--- src/validations.js
+++ src/validations.js
@@ -1,24 +1,24 @@
 module.exports = {
-  required: function (props) {
-    let {value} = props.valueLink
+  required(props) {
+    const { value } = props.valueLink
     // if there is a null option then null is a valid value and there are not
     // any values for which required should return an error
     if ((props.options || []).filter((o) => o.value == null).length > 0) {
       return undefined
     }
-    if (value != null && value !== '') return undefined
-    return 'This field is required.'
+    if (value != null && value !== "") return undefined
+    return "This field is required."
   },
 
-  min: function (props, opts) {
-    let {value} = props.valueLink
-    if (value >= opts || value == null || value === '') return undefined
+  min(props, opts) {
+    const { value } = props.valueLink
+    if (value >= opts || value == null || value === "") return undefined
     return `Value cannot be less than ${opts}`
   },
 
-  max: function (props, opts) {
-    let {value} = props.valueLink
-    if (value <= opts || value == null || value === '') return undefined
+  max(props, opts) {
+    const { value } = props.valueLink
+    if (value <= opts || value == null || value === "") return undefined
     return `Value cannot be greater than ${opts}`
   },
 }
