d26d7e44fe69a739ba8dd883f326b00b7439ee77
Jesse Buchanan <hello@jessebuchanan.ca>
2016-04-06 13:58:16 -0400

    Add positive test for defaultTheme

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,19 @@
 /* global describe, it */
 
 import { expect } from 'chai'
-import { defaultTheme } from '../src/index'
+import { defaultTheme, Form, UnboundInput } from '../src/index'
 
 describe('defaultTheme', () => {
-  it('throws when given something that is not a theme', () => {
+  it('throws when given something that is not an object', () => {
     const fn = defaultTheme.bind(null, 5)
     expect(fn).to.throw(Error, /Invalid Frig theme/)
+  })
+
+  it('assigns the theme to [Form|UnboundInput].originalClass.defaultProps.theme', () => {
+    const theme = {}
+    const result = defaultTheme(theme)
+    expect(result).to.be.true()
+    expect(Form.originalClass.defaultProps.theme).to.equal(theme)
+    expect(UnboundInput.originalClass.defaultProps.theme).to.equal(theme)
   })
 })
