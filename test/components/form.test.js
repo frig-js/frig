297f3cc04990b827d32871a6766f69ddd0313582
Jesse Buchanan <hello@jessebuchanan.ca>
2016-04-11 18:45:58 -0400

    Migrate all old <Form> tests, delete test_old

--- test/components/form.test.js
+++ test/components/form.test.js
@@ -1,4 +1,4 @@
-/* global describe, it */
+/* global describe, it, beforeEach */
 
 import React from 'react'
 import { expect } from 'chai'
@@ -46,11 +46,11 @@ describe('<Form />', () => {
 
   describe('via AbstractForm', () => {
     describe('context', () => {
-      it('passes context down to its children', () => {
-        const UndecoratedForm = Form.originalClass
-        const wrapper = mount(<UndecoratedForm {...formProps} />)
-        const context = wrapper.instance().getChildContext()
+      const UndecoratedForm = Form.originalClass
+      const wrapper = mount(<UndecoratedForm {...formProps} />)
+      const context = wrapper.instance().getChildContext()
 
+      it('passes context (from props) down to its children', () => {
         expect(context.frigForm).to.exist()
         expect(context.frigForm.errors).to.equal(formProps.errors)
         expect(context.frigForm.layout).to.equal(formProps.layout)
@@ -60,18 +60,119 @@ describe('<Form />', () => {
         expect(context.frigForm.data).to.equal(formProps.data)
       })
 
+      it('passes context (internal callbacks) down to its children', () => {
+        expect(context.frigForm.childComponentWillMount).to.be.a('function')
+        expect(context.frigForm.childComponentWillUnmount).to.be.a('function')
+      })
+
+      // pending
       it('passes requestChildComponentChange via context')
-      it('passes childComponentWillMount via context')
-      it('passes childComponentWillUnmount via context')
       it('passes submit via context')
     })
 
-    it('validate()')
-    it('isModified()')
-    it('modifications()')
-    it('resetModified()')
-    it('reset()')
-    it('formData()')
+    describe('public API', () => {
+      const UndecoratedForm = Form.originalClass
+      let wrapper
+      let instance
+
+      beforeEach(() => {
+        wrapper = mount(<UndecoratedForm {...formProps} />)
+        instance = wrapper.instance()
+      })
+
+      describe('validate()', () => {
+        it('should return false if any inputs\' validate() returns false', () => {
+          instance.childComponentWillMount('a', { validate: () => false })
+          instance.childComponentWillMount('b', { validate: () => true })
+          expect(instance.validate()).to.be.false()
+        })
+
+        it('should return true if all inputs\' validate() returns true', () => {
+          instance.childComponentWillMount('a', { validate: () => true })
+          instance.childComponentWillMount('b', { validate: () => true })
+          expect(instance.validate()).to.be.true()
+        })
+      })
+
+      describe('isValid()', () => {
+        it('should return false if any inputs\' isValid() returns false', () => {
+          instance.childComponentWillMount('a', { isValid: () => false })
+          instance.childComponentWillMount('b', { isValid: () => true })
+          expect(instance.isValid()).to.be.false()
+        })
+
+        it('should return true if all inputs\' isValid() returns true', () => {
+          instance.childComponentWillMount('a', { isValid: () => true })
+          instance.childComponentWillMount('b', { isValid: () => true })
+          expect(instance.isValid()).to.be.true()
+        })
+      })
+
+      describe('isModified()', () => {
+        it('should be false initially', () => {
+          expect(instance.isModified()).to.be.false()
+        })
+        it('should be false if all inputs\' isModified() returns false', () => {
+          instance.childComponentWillMount('a', { isModified: () => false })
+          instance.childComponentWillMount('b', { isModified: () => false })
+          expect(instance.isModified()).to.be.false()
+        })
+        it('should be false if any inputs\' isModified() returns true', () => {
+          instance.childComponentWillMount('a', { isModified: () => false })
+          instance.childComponentWillMount('b', { isModified: () => true })
+          expect(instance.isModified()).to.be.true()
+        })
+      })
+
+      describe('resetModified()', () => {
+        it('should call resetModified() on each component', () => {
+          let hasReset = false
+          instance.childComponentWillMount('a', {
+            resetModified: () => { hasReset = true },
+          })
+          instance.resetModified()
+          expect(hasReset).to.be.true()
+        })
+      })
+
+      it('reset()', () => {
+        it('should call reset() on each component', () => {
+          let hasReset = false
+          instance.childComponentWillMount('a', {
+            reset: () => { hasReset = true },
+          })
+          instance.reset()
+          expect(hasReset).to.be.true()
+        })
+      })
+
+      describe('modifications()', () => {
+        describe('when no fields are modified', () => {
+          it('should return empty object', () => {
+            expect(instance.modifications()).to.deep.equal({})
+          })
+        })
+        describe('when fields are modified', () => {
+          it('should return only modified values', () => {
+            instance.childComponentWillMount('a', { isModified: () => true })
+            instance.childComponentWillMount('b', { isModified: () => false })
+            const expected = { a: true }
+            expect(instance.modifications()).to.deep.equal(expected)
+          })
+          it('should return nested modified values', () => {
+            instance.childComponentWillMount('c', {
+              isModified: () => true,
+              modifications: () => ({ d: true, e: true }),
+            })
+            const expected = { c: { d: true, e: true } }
+            expect(instance.modifications()).to.deep.equal(expected)
+          })
+        })
+      })
+
+      it('formData()')
+    })
+
     describe('private', () => {
       // Usually, we wouldn't test private functions so long as they get called
       // via a test exercising the public API. However, these tests were written
