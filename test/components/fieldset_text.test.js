595e0c812f170670a003b47ff70a3101495f5db7
Jason Stathopulos <SpiritBreaker226@users.noreply.github.com>
2016-04-26 13:47:53 -0400

    Add Test For Fieldset Text (#90)

---
+++ test/components/fieldset_text.test.js
@@ -0,0 +1,27 @@
+/* global describe, it, beforeEach */
+
+import React from 'react'
+import { expect } from 'chai'
+import FieldsetText from '../../src/components/fieldset_text'
+import { mount } from 'enzyme'
+
+const defaultContext = {
+  frigFieldset: {
+    index: 1,
+  },
+}
+
+const defaultProps = {
+  text: (index) => `Testing ${index}`,
+}
+
+describe('<FieldsetText />', () => {
+  it('renders an <span> with the text `Testing 1`', () => {
+    const opts = { context: defaultContext }
+    const wrapper = mount(<FieldsetText {...defaultProps} />, opts)
+    const span = wrapper.find('span')
+    const spanProps = span.props()
+
+    expect(spanProps.children).to.equal('Testing 1')
+  })
+})
