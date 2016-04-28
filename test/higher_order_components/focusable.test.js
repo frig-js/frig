89f8241a3b12b3d383c2e1f0318376252fcf23d4
Jesse Buchanan <hello@jessebuchanan.ca>
2016-04-28 15:17:01 -0400

    clean up focusable.test

--- test/higher_order_components/focusable.test.js
+++ test/higher_order_components/focusable.test.js
@@ -10,50 +10,45 @@ import { expect } from 'chai'
 import { mount } from 'enzyme'
 import focusable from '../../src/higher_order_components/focusable.js'
 
-describe('higher order components', () => {
-  class Layout extends React.Component { // eslint-disable-line react/prefer-stateless-function
-    render() {
-      return (<div>
-        <span className="before" />
-        <ExampleFocusable className="focusable" />
-        <span className="after" />
-      </div>)
-    }
+class Layout extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (<div>
+      <span className="before" />
+      <ExampleFocusable className="focusable" />
+      <span className="after" />
+    </div>)
   }
+}
 
-  @focusable
-  class ExampleFocusable extends React.Component { // eslint-disable-line react/no-multi-comp
-    static propTypes = {
-      focused: React.PropTypes.boolean,
-    }
-    render() {
-      return (
-        <div className="theDiv">
-          <input ref="input" className="theInput" />
-          {this.props.focused && <p>Focused!</p>}
-        </div>
-      )
-    }
+@focusable
+class ExampleFocusable extends React.Component { // eslint-disable-line react/no-multi-comp
+  static propTypes = {
+    focused: React.PropTypes.boolean,
   }
+  render() {
+    return (
+      <div className="theDiv">
+        <input ref="input" className="theInput" />
+        {this.props.focused && <p>Focused!</p>}
+      </div>
+    )
+  }
+}
 
-  it('initially is not focused', () => {
-    const wrapper = mount(<Layout />)
-    const example = wrapper.find(ExampleFocusable)
-    expect(example).to.have.length(1)
-    expect(example.prop('focused')).to.not.exist()
-  })
+describe('@focusable decorator', () => {
+  const dom = jsdom('<html><div id="app"></div></html>')
+  usingJsdom(dom, () => {
+    ReactDOM.render(<Layout />, dom.getElementById('app'))
 
-  it('sets props.focused on child component when it is clicked', () => {
-    const dom = jsdom('<html><div id="app"></div></html>')
-    usingJsdom(dom, () => {
-      ReactDOM.render(<Layout />, dom.getElementById('app'))
+    const app = dom.getElementById('app')
+    const input = dom.querySelectorAll('.theInput')[0]
 
-      const app = dom.getElementById('app')
+    it('initially renders child component with props.focused=false', () => {
       expect(app.textContent).to.equal('')
+    })
 
-      const input = dom.querySelectorAll('.theInput')[0]
+    it('sets props.focused=true on child component when it is clicked', () => {
       input.click()
-
       expect(app.textContent).to.equal('Focused!')
     })
   })
